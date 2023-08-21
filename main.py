4c1dc7abca70ff5cc21fd479208ff7ab3b77d6f0
202208521-luis-morales <3683798410101@ingenieria.usac.edu.gt>
2023-08-20 20:06:05 -0600

    Menu terminado
    Solo el orden

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-print("Hello world")
+while(True):
+  print("USAC - Segundo Semestre 2023")
+  print("Proyecto 1 - IPC2")
+  print("Luis Morales - 202208521")
+  print("----------------------------")
+  print("MENU")
+  print("1. Cargar archivo")
+  print("2. Procesar archivo")
+  print("3. Escribir archivo salida")
+  print("4. Mostrar datos del estudiante")
+  print("5. Generar gráfica")
+  option = input("Ingrese el número de opción:")
+
+  if(option == "1"):
+    print("\n")
+    print("# Ha elegido '1. Cargar archivo'")
+    print("\n")
+    print("Ingrese la del archivo")
+    # Subiendo archivo
+    print("\n")
+    print("# Archivo subido con éxito")
+    print("\n")
+  elif(option == "2"):
+    print("\n")
+    print("# Ha elegido '2. Procesar el Archivo'")
+    print("\n")
+    # Procesando
+  elif(option == "3"):
+    print("\n")
+    print("# Ha elegido '3. Escribir Archivo de salida'")
+    print("\n")
+    print("Ingrese la ruta del archivo de salida")
+    # Escribe la ruta
+    print("\n")
+    print("# Ruta ingresada correctamente'")
+    print("\n")
+  elif(option == "4"):
+    print("\n")
+    print("# Ha elegido '4. Mostrar datos del estudiante'")
+    print("\n")
+    print("+-----------------------------------+")
+    print("| Luis Rodrigo Morales Florián      |")
+    print("| 202208521                         |")
+    print("| IPC2 Sección B                    |")
+    print("| Ingeniería en Ciencias y Sistemas |")
+    print("+-----------------------------------+")
+    print("\n")
+  elif(option == "5"):
+    print("\n")
+    print("# Ha elegido '5. Generar gráfica'")
+    print("\n")
+    # Generar gráfica
+  elif(option == "6"):
+    print("\n")
+    print("# ADIOS'")
+    break
+    # Generar gráfica
